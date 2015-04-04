3df1b6791a46d554589eba498f6aa16e275b97a6
Nagarian47 <developer@Mac-de-Developer.local>
2015-04-04 20:44:15 +0200

    Initial commit #2

---
+++ Gamergen RSS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gamergen RSS
+//
+//  Created by Nagarian47 on 04/04/2015.
+//  Copyright (c) 2015 Nagarian47. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
