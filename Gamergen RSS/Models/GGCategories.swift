e6d2c053846d62e68e5bc79bbbc38b76f345f855
Nagarian47 <developer@Mac-de-Developer.local>
2015-04-11 23:08:06 +0200

    - passage en static des éléments composant GGCategories

--- Gamergen RSS/Models/GGCategories.swift
+++ Gamergen RSS/Models/GGCategories.swift
@@ -10,36 +10,32 @@ import Foundation
 import UIKit
 
 struct GGCategories {
-    var array: [GGCategory]
+    static var array = [
+        GGCategory(name: "Global", tag: "global", color: UIColor(netHex:0xEC2F2F)),
+        GGCategory(name: "PS4", tag: "ps4", color: UIColor(netHex:0x8888FF)),
+        GGCategory(name: "XBox One", tag: "xbox-one", color: UIColor(netHex:0x44BB44)),
+        GGCategory(name: "Wii U", tag: "wii-u", color: UIColor(netHex:0x77CCEE)),
+        GGCategory(name: "PS3", tag: "ps3", color: UIColor(netHex:0xAA88EE)),
+        GGCategory(name: "XBox 360", tag: "xbox-360", color: UIColor(netHex:0x99CC33)),
+        GGCategory(name: "Wii", tag: "wii", color: UIColor(netHex:0x66CCCC)),
+        GGCategory(name: "PS Vita", tag: "ps-vita", color: UIColor(netHex:0x6688FF)),
+        GGCategory(name: "2DS/3DS", tag: "n3ds", color: UIColor(netHex:0xFF7733)),
+        GGCategory(name: "PSP", tag: "psp", color: UIColor(netHex:0xFF6666)),
+        GGCategory(name: "DS", tag: "ds", color: UIColor(netHex:0xFF9933)),
+        GGCategory(name: "PC", tag: "pc", color: UIColor(netHex:0x66BBFF)),
+        GGCategory(name: "Android", tag: "android", color: UIColor(netHex:0x66DD44)),
+        GGCategory(name: "iOS", tag: "ios", color: UIColor(netHex:0x77AAEE)),
+        GGCategory(name: "Windows Phone", tag: "windows-phone", color: UIColor(netHex:0x8888EE))]
     
-    init() {
-        array = [GGCategory]()
-        array.append(GGCategory(name: "Global", tag: "global", color: UIColor(netHex:0xEC2F2F)))
-        array.append(GGCategory(name: "PS4", tag: "ps4", color: UIColor(netHex:0x8888FF)))
-        array.append(GGCategory(name: "XBox One", tag: "xbox-one", color: UIColor(netHex:0x44BB44)))
-        array.append(GGCategory(name: "Wii U", tag: "wii-u", color: UIColor(netHex:0x77CCEE)))
-        array.append(GGCategory(name: "PS3", tag: "ps3", color: UIColor(netHex:0xAA88EE)))
-        array.append(GGCategory(name: "XBox 360", tag: "xbox-360", color: UIColor(netHex:0x99CC33)))
-        array.append(GGCategory(name: "Wii", tag: "wii", color: UIColor(netHex:0x66CCCC)))
-        array.append(GGCategory(name: "PS Vita", tag: "ps-vita", color: UIColor(netHex:0x6688FF)))
-        array.append(GGCategory(name: "2DS/3DS", tag: "n3ds", color: UIColor(netHex:0xFF7733)))
-        array.append(GGCategory(name: "PSP", tag: "psp", color: UIColor(netHex:0xFF6666)))
-        array.append(GGCategory(name: "DS", tag: "ds", color: UIColor(netHex:0xFF9933)))
-        array.append(GGCategory(name: "PC", tag: "pc", color: UIColor(netHex:0x66BBFF)))
-        array.append(GGCategory(name: "Android", tag: "android", color: UIColor(netHex:0x66DD44)))
-        array.append(GGCategory(name: "iOS", tag: "ios", color: UIColor(netHex:0x77AAEE)))
-        array.append(GGCategory(name: "Windows Phone", tag: "windows-phone", color: UIColor(netHex:0x8888EE)))
-    }
-    
-    func getCategoryByName(value : String) -> GGCategory? {
-        return array.filter({ (cat) -> Bool in
-            cat.name == value
+    static func getCategoryByName(value : String) -> GGCategory? {
+        return GGCategories.array.filter({ (cat) -> Bool in
+            cat.name.localizedCaseInsensitiveCompare(value) == NSComparisonResult.OrderedSame
         }).first
     }
     
-    func getCategoryByTag(value : String) -> GGCategory? {
-        return array.filter({ (cat) -> Bool in
-            cat.tag == value
+    static func getCategoryByTag(value : String) -> GGCategory? {
+        return GGCategories.array.filter({ (cat) -> Bool in
+            cat.tag.localizedCaseInsensitiveCompare(value) == NSComparisonResult.OrderedSame
         }).first
     }
 }
